ae11d7b46745f62475f6447d1f8f670aee3c3afd
HHHMY <100460092+hmy02@users.noreply.github.com>
2023-10-13 19:14:42 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3|祝|我的公主|生日快乐！|#rectangle|');
+      S.UI.simulate('|#countdown 3|祝|xxx|生日快乐！|#rectangle|');
     }
 
     S.Drawing.loop(function () {
